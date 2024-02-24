afc7f6c6727ba873ad185483fd219f7682e445b3
ouzerzou <mohamed.ouz2016@gmail.com>
2024-02-24 23:31:47 +0100

    new things

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Status of your API"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -16,6 +16,10 @@ app.register_blueprint(app_views)
 def close(err):
     """close storage"""
     storage.close()
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
