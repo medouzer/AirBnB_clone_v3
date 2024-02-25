d230b815664be841361e685ccb858e7f2816cb5c
ouzerzou <mohamed.ouz2016@gmail.com>
2024-02-26 00:08:30 +0100

    new push

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """index """
 
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
+from flask import jsonify
 from models import storage
 
 
