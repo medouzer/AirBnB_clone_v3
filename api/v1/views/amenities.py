3b5b4426ecab333251af7ecb09d5a9a955f77f43
ouzerzou <mohamed.ouz2016@gmail.com>
2024-02-26 20:35:58 +0100

    error

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -4,8 +4,6 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
 
 
