91b3428a331a0807ed65804ae6211f9c4ce1662f
ouzerzou <mohamed.ouz2016@gmail.com>
2024-02-27 02:35:18 +0100

    new things

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -44,26 +44,27 @@ def delete_review(review_id):
     return jsonify({}), 200
 
 
-# @app_views.route('/places/<place_id>/reviews',
-#                  methods=['POST'], strict_slashes=False)
-# def post_review(place_id):
-#     """post a review to a place"""
-#     place = storage.get(Place, place_id)
-#     if place is None:
-#         abort(404)
-#     req_data = request.get_json(silent=True)
-#     if req_data is None:
-#         abort(400, 'Not a JSON')
-#     if "user_id" not in req_data:
-#         abort(400, 'Missing user_id')
-#     user = storage.get(User, req_data["user_id"])
-#     if user not in None:
-#         abort(404)
-#     if "text" not in req_data:
-#         abort(400, 'Missing text')
-#     review = Review(place_id=place_id, **req_data)
-#     review.save()
-#     return jsonify(review.to_dict()), 201
+@app_views.route('/places/<place_id>/reviews',
+                 methods=['POST'], strict_slashes=False)
+def post_review(place_id):
+    """post a review to a place"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    req_data = request.get_json(silent=True)
+    if req_data is None:
+        abort(400, 'Not a JSON')
+    user_id = req_data.get("user_id")
+    if user_id is None:
+        abort(400, 'Missing user_id')
+    user = storage.get(User, req_data["user_id"])
+    if user not in None:
+        abort(404)
+    if "text" not in req_data:
+        abort(400, 'Missing text')
+    review = Review(place_id=place_id, **req_data)
+    review.save()
+    return jsonify(review.to_dict()), 201
 
 
 @app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
@@ -75,9 +76,9 @@ def put_review(review_id):
     req_data = request.get_json(silent=True)
     if req_data is None:
         abort(400, 'Not a JSON')
+    fix_keys = ["id", "user_id", "place_id", "created_at", "updated_at"]
     for key, value in req_data.items():
-        if key not in ["id", "user_id", "place_id",
-                       "created_at", "updated_at"]:
+        if key not in fix_keys:
             setattr(req_data, key, value)
     req_data.save()
     return jsonify(req_data.to_dict()), 200
